7d3a454d76969ee27176ee03e15d38066fd90557
Daniel Price <dprice1@dt-login01.delta.ncsa.illinois.edu>
2025-08-19 16:21:52 -0500

    update

--- llama_2_70_scripts/llama_2_70b_pwl_act_2.sh
+++ llama_2_70_scripts/llama_2_70b_pwl_act_2.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --time=8:00:00
 #SBATCH --cpus-per-task=32
-#SBATCH --gres=gpu:8
-#SBATCH --partition=highgpu
+#SBATCH --gres=gpu:2
+#SBATCH --constraint=h100
 #SBATCH --job-name=llama_2_70b_profiling
 #SBATCH --error=error/llama_2/llama_2_70b_error.txt
 #SBATCH --output=output/llama_2/llama_2_70b_output.txt
