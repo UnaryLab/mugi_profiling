0669e16aa19b51d4b5584805f8d141c4c34152ea
Daniel Price <dprice1@dt-login01.delta.ncsa.illinois.edu>
2025-08-18 23:35:23 -0500

    update

--- whisper_scripts/run_whisper_tiny.sh
+++ whisper_scripts/run_whisper_tiny.sh
@@ -25,7 +25,7 @@ cd ~/mugi_profiling
 
 # Configuration files to process
 model_configs=("config/model_config/whisper/whisper_tiny.yaml")
-nonlinear_config="config/nonlinear_config/large_model_configs/nonlinear_config_torch.yaml"
+nonlinear_config="config/nonlinear_config/nonlinear_config.yaml"
 parameter_config="config/parameter_config/parameter_config.yaml"
 hf_token="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"
 
