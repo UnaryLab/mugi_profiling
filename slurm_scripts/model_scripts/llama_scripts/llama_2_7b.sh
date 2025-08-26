49dee96dabbdf555e6800a2d61d0eb5193677ecc
daniel <daniel.pricd@ucf.edu>
2025-08-26 13:02:17 -0400

    push to newton cluster

--- slurm_scripts/model_scripts/llama_scripts/llama_2_7b.sh
+++ slurm_scripts/model_scripts/llama_scripts/llama_2_7b.sh
@@ -11,11 +11,8 @@
 #SBATCH --output=output/llama_2/llama_2_7b_output.txt
 
 #module load python
-echo "before"
-module load anaconda
-echo "after"
-
-module load cuda
+#module load anaconda
+#module load cuda
 
 # Initialize conda properly for bash script
 # source $(conda info --base)/etc/profile.d/conda.sh
