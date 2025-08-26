10fe3104616b1b1523d054952c3cc2e4e5e577a0
daniel <daniel.pricd@ucf.edu>
2025-08-26 12:55:30 -0400

    push to newton cluster

--- slurm_scripts/model_scripts/llama_scripts/llama_2_7b.sh
+++ slurm_scripts/model_scripts/llama_scripts/llama_2_7b.sh
@@ -15,8 +15,8 @@ module load anaconda
 module load cuda
 
 # Initialize conda properly for bash script
-#source $(conda info --base)/etc/profile.d/conda.sh
-eval "$(conda shell.bash hook)"
+source $(conda info --base)/etc/profile.d/conda.sh
+#eval "$(conda shell.bash hook)"
 
 conda activate mugi_profiling
 
