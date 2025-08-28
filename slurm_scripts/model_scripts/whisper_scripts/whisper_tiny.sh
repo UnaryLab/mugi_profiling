b75ae93549fd77baa304171468e570ecf4f478ca
daniel <daniel.pricd@ucf.edu>
2025-08-28 12:25:47 -0400

    push to newton cluster

--- slurm_scripts/model_scripts/whisper_scripts/whisper_tiny.sh
+++ slurm_scripts/model_scripts/whisper_scripts/whisper_tiny.sh
@@ -20,7 +20,7 @@ conda activate mugi_profiling
 cd ~/mugi_profiling
 
 # Configuration files to process
-model_configs=("config/model_config/whisper/whisper_tiny.yaml")
+model_configs="config/model_config/whisper/whisper_tiny.yaml"
 nonlinear_config="config/nonlinear_config/large_model_configs/nonlinear_config_torch.yaml"
 parameter_config="config/parameter_config/parameter_config.yaml"
 hf_token="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"
