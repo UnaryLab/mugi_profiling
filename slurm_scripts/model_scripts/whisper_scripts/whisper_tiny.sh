256a980f3b271c8f33b9bf23e4d755aeaa834ec6
daniel <daniel.pricd@ucf.edu>
2025-09-02 12:30:49 -0400

    push to newton cluster

--- slurm_scripts/model_scripts/whisper_scripts/whisper_tiny.sh
+++ slurm_scripts/model_scripts/whisper_scripts/whisper_tiny.sh
@@ -35,13 +35,13 @@ echo "----------------------------------------"
 # Run the transformer script with the current config
 export PYTHONPATH=~/mugi_profiling:$PYTHONPATH
 
-deepspeed --num_nodes=1 --num_gpus=2 src/model_script.py --model_config "$model_config" \
-                            --nonlinear_config "$nonlinear_config" \
-                            --parameter_config "$parameter_config"
-
-# python src/model_script.py --model_config "$model_config" \
+# deepspeed --num_nodes=1 --num_gpus=2 src/model_script.py --model_config "$model_config" \
 #                             --nonlinear_config "$nonlinear_config" \
 #                             --parameter_config "$parameter_config"
+
+python src/model_script.py --model_config "$model_config" \
+                            --nonlinear_config "$nonlinear_config" \
+                            --parameter_config "$parameter_config"
 
 # Capture the exit code
 exit_code=$?
