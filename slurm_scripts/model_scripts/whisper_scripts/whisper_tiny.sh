ebd6ed82f52308189d482bc26ab9f32f960a9516
daniel <daniel.pricd@ucf.edu>
2025-09-02 10:58:37 -0400

    push to newton cluster

--- slurm_scripts/model_scripts/whisper_scripts/whisper_tiny.sh
+++ slurm_scripts/model_scripts/whisper_scripts/whisper_tiny.sh
@@ -13,7 +13,7 @@ module load cuda
 
 # Initialize conda properly for bash script
 # source $(conda info --base)/etc/profile.d/conda.sh
-eval "$(conda shell.bash hook)"
+#eval "$(conda shell.bash hook)"
 
 conda activate mugi_profiling
 
