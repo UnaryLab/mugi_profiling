f8847aa1d3f57b70fb755bf4389791e5cd410fd3
daniel <daniel.pricd@ucf.edu>
2025-09-05 10:58:09 -0400

    push to newton cluster

--- src/inference_classes/llama_inference.py
+++ src/inference_classes/llama_inference.py
@@ -48,7 +48,7 @@ class LlamaModel(InferenceModel):
         if self.tokenizer.pad_token is None:
             self.tokenizer.pad_token = self.tokenizer.eos_token
 
-        self.model = AutoModelForCausalLM.from_pretrained(self.model_name, torch_dtype=torch.float16, attn_implementation='eager', device=self.device, use_cache=False)
+        self.model = AutoModelForCausalLM.from_pretrained(self.model_name, torch_dtype=torch.float16, attn_implementation='eager', device_map={"":"cuda:0"}, use_cache=False)
         self.max_length = self.model.config.max_position_embeddings
 
         if self.max_length > 4096:
