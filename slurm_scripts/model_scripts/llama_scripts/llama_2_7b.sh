47ac220c6fd37f23ddc06886469099c235701649
daniel <daniel.pricd@ucf.edu>
2025-09-05 14:31:26 -0400

    push to newton cluster

--- slurm_scripts/model_scripts/llama_scripts/llama_2_7b.sh
+++ slurm_scripts/model_scripts/llama_scripts/llama_2_7b.sh
@@ -37,7 +37,7 @@ echo "----------------------------------------"
 # Run the transformer script with the current config
 export PYTHONPATH=~/mugi_profiling:$PYTHONPATH
 
-python src/model_script.py --model_config "$model_config" \
+python -m src.model_script --model_config "$model_config" \
                             --nonlinear_config "$nonlinear_config" \
                             --parameter_config "$parameter_config"
 
