b48a94be567a948965b8ec7b26080bfb4eaa613a
daniel <daniel.pricd@ucf.edu>
2025-09-05 15:44:34 -0400

    push to newton cluster

--- src/inference_classes/llama_inference.py
+++ src/inference_classes/llama_inference.py
@@ -109,5 +109,5 @@ class LlamaModel(InferenceModel):
                                 self.max_length - 1]
         
     def tp_patch(self):
-        print(self.model.embed_tokens)
+        self.model.model = LlamaModel(self.model.config)
         exit()
