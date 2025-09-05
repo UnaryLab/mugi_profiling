1106020c9e6c491590e7feea7b223ca0a340dd14
daniel <daniel.pricd@ucf.edu>
2025-09-05 15:52:53 -0400

    push to newton cluster

--- src/custom_models/llama_tp.py
+++ src/custom_models/llama_tp.py
@@ -19,7 +19,9 @@ class LlamaModel(LlamaPreTrainedModel):
         self.padding_idx = config.pad_token_id
         self.vocab_size = config.vocab_size
 
+        
         self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
+        print(self.embed_tokens.shape)
         self.layers = nn.ModuleList(
             [LlamaDecoderLayer(config, layer_idx) for layer_idx in range(config.num_hidden_layers)]
         )
