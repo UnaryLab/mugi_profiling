6ec8923429b47f92d97dad522b615a739977f778
daniel <daniel.pricd@ucf.edu>
2025-09-05 15:58:37 -0400

    push to newton cluster

--- src/custom_models/llama_tp.py
+++ src/custom_models/llama_tp.py
@@ -19,9 +19,18 @@ class LlamaModel(LlamaPreTrainedModel):
         self.padding_idx = config.pad_token_id
         self.vocab_size = config.vocab_size
 
+        n_gpus = torch.cuda.device_count()
+
+        # embed_tokes = [vocab_size, hidden_size]
+        # self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
         
-        self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
-        print(self.embed_tokens.weight.shape)
+        self.embed_token_slices = nn.ModuleList(
+            [nn.Embedding(config.vocab_size, config.hidden_size // n_gpus, self.padding_idx) for i in range(n_gpus)]
+        )
+        print(n_gpus)
+        for embed_token in self.embed_token_slices:
+            print(embed_token.weight.shape)
+
         self.layers = nn.ModuleList(
             [LlamaDecoderLayer(config, layer_idx) for layer_idx in range(config.num_hidden_layers)]
         )
