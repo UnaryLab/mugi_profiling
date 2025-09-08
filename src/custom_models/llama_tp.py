d4c0523aee664b13a1d98e8a5692cdc6a5789e9d
daniel <daniel.pricd@ucf.edu>
2025-09-08 18:20:32 -0400

    push to newton cluster

--- src/custom_models/llama_tp.py
+++ src/custom_models/llama_tp.py
@@ -64,11 +64,12 @@ def patch_linear(linear, bias, world_size, rank):
     local_weight = linear.weight[start:end, :].contiguous()
     local_linear = nn.Linear(k, n_per_gpu, bias=bias, dtype=linear.weight.dtype)
     print(local_weight.dtype)
-    exit()
+    
     with torch.no_grad():
         local_linear.weight.copy_(local_weight)
     local_linear = local_linear.cuda()
     print(local_linear.weight.dtype)
+    exit()
 
     return local_linear
     
