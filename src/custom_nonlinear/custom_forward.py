9cbd5e9e20361d380dc142a6ccedbb56c7abe435
daniel <daniel.pricd@ucf.edu>
2025-09-08 15:33:16 -0400

    push to newton cluster

--- src/custom_nonlinear/custom_forward.py
+++ src/custom_nonlinear/custom_forward.py
@@ -29,6 +29,8 @@ def llama_forward(llama_eager):
 
         print('preprojection')
         print('q')
+        print(hidden_states.device, hidden_states.shape)
+        print(self.q_proj.weight.device, self.q_proj.weight.shape)
         query_states = self.q_proj(hidden_states).view(hidden_shape).transpose(1, 2)
         print('k')
         key_states = self.k_proj(hidden_states).view(hidden_shape).transpose(1, 2)
