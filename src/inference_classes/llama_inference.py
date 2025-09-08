34616bfc84a529f623a2ddfb7fae1c868d6716f4
daniel <daniel.pricd@ucf.edu>
2025-09-08 14:54:53 -0400

    push to newton cluster

--- src/inference_classes/llama_inference.py
+++ src/inference_classes/llama_inference.py
@@ -93,8 +93,8 @@ class LlamaInference(InferenceModel):
                     self.inputs.append(tokenized_example)
 
     def run_inference(self, batch):
-        input_ids = torch.stack([ex["input_ids"] for ex in batch]).to(self.device)
-        attention_mask = torch.stack([ex["attention_mask"] for ex in batch]).to(self.device).bool()
+        input_ids = torch.stack([ex["input_ids"] for ex in batch]).to(self.model.model.embed_tokens.weight.device)
+        attention_mask = torch.stack([ex["attention_mask"] for ex in batch]).to(self.model.model.embed_tokens.weight.device).bool()
 
         with torch.inference_mode():
             outputs = self.model(input_ids=input_ids, attention_mask=attention_mask, labels=input_ids, use_cache=False)
