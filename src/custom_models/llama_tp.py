eda006013387c001eab39589d1bacc3f2092269c
daniel <daniel.pricd@ucf.edu>
2025-09-08 12:49:08 -0400

    push to newton cluster

--- src/custom_models/llama_tp.py
+++ src/custom_models/llama_tp.py
@@ -5,11 +5,11 @@ import torch.distributed as dist
 def init_dist():
     dist.init_process_group(backend='nccl')
     torch.cuda.set_device(dist.get_rank() % torch.cuda.device_count())
-    print("Current CUDA device index:", torch.cuda.current_device())
-    return dist.get_world_size()
+    return dist.get_world_size(), dist.get_rank()
     
-def patch_embedding(model, world_size):
+def patch_embedding(model, world_size, rank):
     print(model.model.embed_tokens)
+    print(world_size, rank)
     embed_tokens = model.model.embed_tokens
     vocab_size, embed_dim = embed_tokens.weight.shape
     exit()
