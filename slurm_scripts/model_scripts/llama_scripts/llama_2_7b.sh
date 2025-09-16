ae265b2df01ca21936097576a50a92e8f36df12a
daniel <daniel.pricd@ucf.edu>
2025-09-16 12:30:45 -0400

    push to newton cluster

--- slurm_scripts/model_scripts/llama_scripts/llama_2_7b.sh
+++ slurm_scripts/model_scripts/llama_scripts/llama_2_7b.sh
@@ -23,7 +23,7 @@ cd ~/mugi_profiling
 
 # Configuration files to process
 model_config="config/model_config/llama/llama_2_7b.yaml"
-nonlinear_config="config/nonlinear_config/vlp/vlp_softmax_layers.yaml"
+nonlinear_config="config/nonlinear_config/vlp/vlp_softmax.yaml"
 parameter_config="config/parameter_config/parameter_config.yaml"
 hf_token="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"
 
