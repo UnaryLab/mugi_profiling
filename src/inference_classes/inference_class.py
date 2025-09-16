4171ee1f7bf9011a6f9c25b02790819cb6557bc3
daniel <daniel.pricd@ucf.edu>
2025-09-16 19:09:21 -0400

    push to newton cluster

--- src/inference_classes/inference_class.py
+++ src/inference_classes/inference_class.py
@@ -219,13 +219,13 @@ class InferenceModel(ABC):
                 for subkey, subvalue in value.items():
                     if subkey == 'value':
                         assert self.patched_layer not in attn_layer_params, "Multiple 'value' keys found in attention parameters."
-                        attn_layer_params[str(self.patched_layer)] = {key: subvalue}
+                        attn_layer_params[self.patched_layer] = {key: subvalue}
                         attn_layer_value = subvalue
                     elif subkey == 'default':
                         attn_default_params = {key: subvalue}
                     else:
                         assert subkey not in attn_layer_params, "Multiple 'value' keys found in attention parameters."
-                        attn_layer_params[str(subkey)] = {key: subvalue}
+                        attn_layer_params[subkey] = {key: subvalue}
 
         for key, value in ffn_params.items():
             if not isinstance(value, dict):
@@ -235,25 +235,25 @@ class InferenceModel(ABC):
                 for subkey, subvalue in value.items():
                     if subkey == 'value':
                         assert self.patched_layer not in ffn_layer_params, "Multiple 'value' keys found in FFN parameters."
-                        ffn_layer_params[str(self.patched_layer)] = {key: subvalue}
+                        ffn_layer_params[self.patched_layer] = {key: subvalue}
                         ffn_layer_value = subvalue
                     elif subkey == 'default':
                         ffn_default_params = {key: subvalue}
                     else:
                         assert subkey not in ffn_layer_params, "Multiple 'value' keys found in FFN parameters."
-                        ffn_layer_params[str(subkey)] = {key: subvalue}
+                        ffn_layer_params[subkey] = {key: subvalue}
 
         if attn_params:
             for i, attn_layer in enumerate(self.attention_objects):
                 if i+1 in attn_layer_params:
-                    attn_layer.set_params(**attn_global_params, **attn_layer_params, config_path=attn_config_path)
+                    attn_layer.set_params(**attn_global_params, **attn_layer_params[i+1], config_path=attn_config_path)
                 else:
                     attn_layer.set_params(**attn_global_params, **attn_default_params, config_path=attn_config_path)
 
         if ffn_params:
             for i, ffn_layer in enumerate(self.ffn_objects):
-                if i == self.patched_layer:
-                    ffn_layer.set_params(**ffn_global_params, **ffn_layer_params, config_path=ffn_config_path)
+                if i+1 in ffn_layer_params:
+                    ffn_layer.set_params(**ffn_global_params, **ffn_layer_params[i+1], config_path=ffn_config_path)
                 else:
                     ffn_layer.set_params(**ffn_global_params, **ffn_default_params, config_path=ffn_config_path)
 
