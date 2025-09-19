d35a5e166c6922b18af7af62ee9e1284bfc2c981
daniel <daniel.pricd@ucf.edu>
2025-09-19 11:51:34 -0400

    push to newton cluster

--- src/inference_classes/inference_class.py
+++ src/inference_classes/inference_class.py
@@ -56,8 +56,8 @@ class InferenceModel(ABC):
 
         self.patch_per_layer = self.nonlinear_dict.get('patch_per_layer', False)
         self.patched_layer = self.nonlinear_dict.get('patched_layer', None)
-        self.patched_layer = self.patched_layer - 1 if self.patched_layer is not None else None
         self.patched_layer_path = f'{self.patched_layer}/' if self.patched_layer is not None else ''
+        self.patched_layer = self.patched_layer - 1 if self.patched_layer is not None else None
         
         # Initialize DataFrame for collecting results
         self.df = None
