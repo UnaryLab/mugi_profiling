6297133e9d87f37797cedd67d266b7d0f1f80521
daniel <daniel.pricd@ucf.edu>
2025-10-07 15:04:38 -0400

    push to newton cluster

--- slurm_scripts/model_scripts/llama_scripts/llama_2_13b.sh
+++ slurm_scripts/model_scripts/llama_scripts/llama_2_13b.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --time=01:00:00
-#SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:8
 #SBATCH --job-name=llama_2_13b
 #SBATCH --partition=highgpu
 #SBATCH --job-name=llama_2_7b_profiling
