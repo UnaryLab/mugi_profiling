a182be7fb9106e2b6c4efcf3890e73ea63a311a1
daniel <daniel.pricd@ucf.edu>
2025-10-22 19:09:10 -0400

    push to newton cluster

--- slurm_scripts/model_scripts/llama_scripts/llama_2_70b.sh
+++ slurm_scripts/model_scripts/llama_scripts/llama_2_70b.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --time=04:00:00
 #SBATCH --cpus-per-task=64
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:4
 #SBATCH --job-name=llama_2_70b
 #SBATCH --partition=highgpu
 #SBATCH --error=output/run/llama_2/llama_2_70b/error.txt
